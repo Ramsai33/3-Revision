6ffc674b2d6d658417a2c0c31119b67a815b11fa
mudur <mudurukollaramsai335@gmail.com>
2024-03-20 05:18:50 +0530

    catalogue

--- shell-roboshop/mongo.sh
+++ shell-roboshop/mongo.sh
@@ -1,25 +1,25 @@
 source common.sh
 
 print_head "Copying Mongo file"
-cp ${script_location}/files/mongo.conf /etc/yum.repos.d/mongo.repo
+cp ${script_location}/files/mongo.conf /etc/yum.repos.d/mongo.repo &>>${log}
 status
 
 print_head "Install Mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log}
 status
 
 print_head "Enable Mongo"
-systemctl enable mongod
+systemctl enable mongod &>>${log}
 status
 
 print_head"Start Mongo"
-systemctl start mongod
+systemctl start mongod &>>${log}
 status
 
 print_head "Change Port"
-sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf &>>${log}
 status
 
 print_head "Restart"
-systemctl restart mongod
+systemctl restart mongod &>>${log}
 status
