5612ce2ab3e840d38479b3a6fac99c83acad196e
mudur <mudurukollaramsai335@gmail.com>
2024-03-19 09:40:47 +0530

    Frontend

---
+++ shell-roboshop/mongo.sh
@@ -0,0 +1,13 @@
+script_location=$(pwd)
+
+cp ${script_location}files/mongo.conf /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+
+systemctl start mongod
+
+sed -i -e 's/0.0.0.0/127.0.0.1/gi' /etc/mongod.conf
+
+systemctl restart mongod
