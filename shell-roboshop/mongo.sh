ef851cd5b5f140ef6107f9ce103a15d86983c50c
mudur <mudurukollaramsai335@gmail.com>
2024-03-19 09:51:29 +0530

    Mongo

--- shell-roboshop/mongo.sh
+++ shell-roboshop/mongo.sh
@@ -8,6 +8,6 @@ systemctl enable mongod
 
 systemctl start mongod
 
-sed -i -e 's/0.0.0.0/127.0.0.1/gi' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf
 
 systemctl restart mongod
