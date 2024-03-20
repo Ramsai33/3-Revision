a6d981e398bbb4c0a3a7b7cf7571f7657c34ddc6
mudur <mudurukollaramsai335@gmail.com>
2024-03-20 06:46:13 +0530

    catalogue

--- shell-roboshop/catalogue.sh
+++ shell-roboshop/catalogue.sh
@@ -1,30 +1,3 @@
 source common.sh
+nodejs
 
-print_head "Disable Repo"
-yum module disable nodejs -y &>>${log}
-status
-
-print_head "Enable Nodejs"
-yum module enable nodejs:18 -y &>>${log}
-status
-
-print_head "InstallNodejs"
-yum install nodejs -y &>>${log}
-status
-
-AppPre_req
-
-print_head "Chnge dir"
-cd /app &>>${log}
-status
-print_head "NPM Install"
-npm install &>>${log}
-status
-
-print_head "Copy File"
-cp ${script_location}/files/catalogue.conf /etc/systemd/system/catalogue.service &>>${log}
-status
-
-systemd
-
-schema_load
